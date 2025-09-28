c12b311e819ff9532c323cf584de7a045c6473e7
jahidhasan136 <jh3495689@gmail.com>
2025-09-29 00:21:26 +0600

    breakpoint or font setup

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="xs:hidden md:flex">This is common layout</h1>
+    </div>
+  );
+};
+
+export default page;
